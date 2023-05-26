3440780866b97e704b8da92c9b2d7949c33e2cd7
boudewijn overvliet <boudewijn@hello-data.nl>
2023-05-26 21:29:26 +0200

    added user job

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Queue\QueueManager;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UsersController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        return new JsonResponse([
+            'users' => User::query()->get(),
+        ]);
+    }
+
+    public function create(QueueManager $queueManager): JsonResponse
+    {
+        $queueManager->push(new \App\Jobs\CreateUserJob());
+
+        return new JsonResponse([
+            'message' => 'User created',
+        ]);
+    }
+}
---
+++ app/Jobs/CreateUserJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CreateUserJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        User::query()->create([
+            'name' => 'John Doe',
+            'email' => 'bob'.\mt_rand().'@bob.nl',
+            'password' => 'password',
+        ]);
+
+    }
+}
