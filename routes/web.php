6eada4736fcf531308d14ce06e6865575e5c3982
boudewijn overvliet <boudewijn@hello-data.nl>
2023-05-26 21:29:20 +0200

    added user job

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return "Hello World";
-});
+Route::post('/users', \App\Http\Controllers\UsersController::class.'@create');
+Route::get('/users', \App\Http\Controllers\UsersController::class.'@index');
